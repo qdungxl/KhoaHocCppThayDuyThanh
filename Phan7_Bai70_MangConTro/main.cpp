bb791169a82e5748a5911f8d99522cfb3067e4d3
Dung-CoDienTu <maiquocdung95@gmail.com>
2022-04-10 10:19:04 +0900

    Phan7,Bai70,Mang con tro

---
+++ Phan7_Bai70_MangConTro/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	int *p[5];
+	for(int i=0;i<5;i++){
+		p[i] = new int; //cap phat bo nho. 
+		*p[i] = i*2;
+	}
+	for(int i=0; i<5;i++){
+		cout<<p[i]<<"=>"<<*p[i]<<endl;
+	}
+	return 0;
+}
