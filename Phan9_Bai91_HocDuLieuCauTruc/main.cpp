dd7a71502f0dfad568590bc098f3a549c328d719
Dung-CoDienTu <maiquocdung95@gmail.com>
2022-04-11 20:55:56 +0900

    Phan9,Bai91, Khai bao cau truc

---
+++ Phan9_Bai91_HocDuLieuCauTruc/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+struct DiaChi{
+	char SoNha[25];
+	char TenDuong[250];
+	char Quan[25];
+	char TinhThanh[25];
+};
+struct SinhVien{
+	char MaSV[10];
+	char TenSV[250];
+	bool GioiTinh;
+	DiaChi DiaChiNha;
+}teo,ty;
+int main(int argc, char** argv) {
+	
+	return 0;
+}
