26bc026640a673538b4a0f96c94482c7b1b6c3fa
Dung-CoDienTu <maiquocdung95@gmail.com>
2022-04-11 21:04:04 +0900

    Phan9,Bai92, Truy cap cac thanh vien trong bien cau truc

--- Phan9_Bai91_HocDuLieuCauTruc/main.cpp
+++ Phan9_Bai91_HocDuLieuCauTruc/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <string.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 using namespace std;
 struct DiaChi{
@@ -15,6 +15,14 @@ struct SinhVien{
 	DiaChi DiaChiNha;
 }teo,ty;
 int main(int argc, char** argv) {
+	strcpy(teo.TenSV,"Nguyen Van Teo");
+	teo.GioiTinh = true;
+	strcpy(teo.MaSV,"SV113");
+	cout<<"---------------"<<endl;
+	cout<<teo.MaSV<<" - "<<teo.TenSV<<" - "<<(teo.GioiTinh==true?"Nam":"Nu")<<endl;
 	
+	SinhVien bin;
+	strcpy(bin.TenSV,"Pham Bin");
+	cout<<bin.TenSV<<endl;
 	return 0;
 }
